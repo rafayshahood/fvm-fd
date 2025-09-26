33656fbac1c2f2d629616a8c8877d71be77c9cd9
Rafay <rafay@Rafays-MacBook-Air.local>
2025-09-26 15:01:12 +0500

    -

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 // api.js
-const RUNPOD_DEFAULT = 'https://subjects-emma-salon-backed.trycloudflare.com';
+const RUNPOD_DEFAULT = 'https://hills-listprice-horizontal-magnetic.trycloudflare.com';
 const LOCAL_DEFAULT  = 'http://localhost:8888';
 
