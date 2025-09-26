9447b0f5a1c9db6a36ff9475b3d1423c3dc8f009
Rafay <rafay@Rafays-MacBook-Air.local>
2025-09-26 10:39:01 +0500

    -

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // api.js
-const RUNPOD_DEFAULT = 'https://6xk1f81dfjz1s8-8888.proxy.runpod.net';
+const RUNPOD_DEFAULT = 'https://2baj3omlbmnbh5-8888.proxy.runpod.net';
 const LOCAL_DEFAULT  = 'http://localhost:8888';
 
 // Priority:
